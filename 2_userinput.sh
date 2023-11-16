8a9924af2ef563d69bd1aec1b82dd18f591cd52e
iamvikramkumar <iamvikramkumar5@gmail.com>
2023-11-16 17:53:45 +0530

    user_input

---
+++ 2_userinput.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Enter Your Name: "
+read name
+echo "Namaste! I am $name! from BHARAT. "
+
+
+
+# Prompt the user for their name
+# read -p "Enter your name: " name
+
+# # Display a greeting using the entered name
+# echo "Hello, $name! Welcome to the script."
