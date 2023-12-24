a7e0ac8af32b38d88bbfbf1292e136919a65551b
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-12-24 11:52:37 +0000

    elif

---
+++ 4_elif.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+a=1000
+b=200
+c=300
+
+if [[ $a -gt $b && $a -gt $c ]]
+then
+echo "A is biggest"
+elif [[ $b -gt $a && $b -gt $c ]]
+then 
+echo "B is biggest"
+else
+echo "C is biggest"
+
+fi
+
