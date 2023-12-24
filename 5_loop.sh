623445404fe4945f6aeb236fd887b7ff22dee600
Vikram Kumar <89016145+iamvikramkumar@users.noreply.github.com>
2023-12-24 12:10:32 +0000

    loop

--- 5_loop.sh
+++ 5_loop.sh
@@ -4,3 +4,18 @@ for((i=0; i < 10; i++))
 do
 echo "$i"
 done
+
+
+# For iterating of files 
+# Below code gives you all files available in the folder
+# for FILE in *
+# do 
+# echo  $FILE
+# done
+
+
+# Below code only gives text files
+for FILE in *.txt
+do 
+echo  $FILE
+done
